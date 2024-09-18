f8eb72e0de031ab15ae09ebb794856c69d1de00c
niketjain1 <niketj2000@gmail.com>
2024-09-18 20:16:12 +0530

    updated the models and added actionrequest model

--- src/models.py
+++ src/models.py
@@ -6,3 +6,8 @@ class VideoRequest(BaseModel):
 class QuestionRequest(BaseModel):
     url: str
     question: str
+    chat_history: list
+
+class ActionRequest(BaseModel):
+    url: str
+    action: str
